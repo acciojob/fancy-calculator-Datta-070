1df8a073af19dd591309d81c3e59b7eafbec6357
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-27 12:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+
+let op1=prompt("Enter the op1");
+let var1=parseInt(op1)*2;
+let op2=prompt("Enter the op2");
+let var2=parseInt(op2);
+let result=var1+var2;
+alert("The final result is: "+result);
